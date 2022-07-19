14a57328909e01df1cd144106d27a2d339d8d094
belbix <7453635@gmail.com>
2022-07-19 12:52:49 +0300

    add bsc addresses

--- src/mappings/constants.ts
+++ src/mappings/constants.ts
@@ -22,9 +22,7 @@ export function wethAddress(): Address {
   if (network == 'matic') {
     return Address.fromString('0x7ceb23fd6bc0add59e62ac25578270cff1b9f619');
   } else if (network == 'bsc') {
-    return Address.fromString(ADDRESS_ZERO); //todo
-  } else if (network == 'fuji') {
-    return Address.fromString(ADDRESS_ZERO);//todo
+    return Address.fromString('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c');
   } else {
     log.critical("UNKNOWN NETWORK {}", [network])
     return Address.fromString(ADDRESS_ZERO);
@@ -35,9 +33,7 @@ export function usdcAddress(): Address {
   if (network == 'matic') {
     return Address.fromString('0x2791bca1f2de4661ed88a30c99a7a9449aa84174');
   } else if (network == 'bsc') {
-    return Address.fromString(ADDRESS_ZERO); //todo
-  } else if (network == 'fuji') {
-    return Address.fromString(ADDRESS_ZERO);//todo
+    return Address.fromString('0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d');
   } else {
     log.critical("UNKNOWN NETWORK {}", [network])
     return Address.fromString(ADDRESS_ZERO);
@@ -48,9 +44,7 @@ export function usdcWethPairAddress(): Address {
   if (network == 'matic') {
     return Address.fromString('0xce1923d2242bba540f1d56c8e23b1fbeae2596dc');
   } else if (network == 'bsc') {
-    return Address.fromString(ADDRESS_ZERO); //todo
-  } else if (network == 'fuji') {
-    return Address.fromString(ADDRESS_ZERO);//todo
+    return Address.fromString('0x89B26AF36fA8705A27934fcED56D154BDA01315a');
   } else {
     log.critical("UNKNOWN NETWORK {}", [network])
     return Address.fromString(ADDRESS_ZERO);
@@ -73,6 +67,17 @@ export function whitelisted(): Address[] {
       Address.fromString('0x580A84C73811E1839F75d86d75d88cCa0c241fF4'), // QI
       Address.fromString('0x255707b70bf90aa112006e1b07b9aea6de021424'), // TETU
     ]
+  } else if (network == 'bsc') {
+    return [
+      '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // WBNB
+      '0x2170ed0880ac9a755fd29b2688956bd959f933f8', // WETH
+      '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', // USDC
+      '0x90c97f71e18723b0cf0dfa30ee176ab653e89f40', // FRAX
+      '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3', // DAI
+      '0x55d398326f99059ff775485246999027b3197955', // USDT
+      '0x3f56e0c36d275367b8c502090edf38289b3dea0d', // MAI
+      '0xe9e7cea3dedca5984780bafc599bd69add087d56', // BUSD
+    ];
   } else {
     log.critical("UNKNOWN NETWORK {}", [network])
     return [Address.fromString(ADDRESS_ZERO)];
@@ -88,6 +93,15 @@ export function stablecoins(): Address[] {
       Address.fromString('0xa3Fa99A148fA48D14Ed51d610c367C61876997F1'), // MAI
       Address.fromString('0x45c32fA6DF82ead1e2EF74d17b76547EDdFaFF89'), // FRAX
       Address.fromString('0x236eeC6359fb44CCe8f97E99387aa7F8cd5cdE1f'), // USD+
+    ]
+  } else if (network == 'bsc') {
+    return [
+      '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', // USDC
+      '0x90c97f71e18723b0cf0dfa30ee176ab653e89f40', // FRAX
+      '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3', // DAI
+      '0x55d398326f99059ff775485246999027b3197955', // USDT
+      '0x3f56e0c36d275367b8c502090edf38289b3dea0d', // MAI
+      '0xe9e7cea3dedca5984780bafc599bd69add087d56', // BUSD
     ]
   } else {
     log.critical("UNKNOWN NETWORK {}", [network])
